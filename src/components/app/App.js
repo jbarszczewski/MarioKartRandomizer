851c723b31cfe3bcb86140567534b63a770e2fd9
Jakub Barszczewski <jakub.barszczewski@outlook.com>
2018-08-12 09:28:58 +0100

    moved conditions to app

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,22 +1,56 @@
-import React, { Component } from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-
-import Menu from './../menu/Menu'
-import Conditions from './../conditions/Conditions'
+import React, { Component } from 'react';
+import Item from '../shared/Item';
+import Button from '../shared/Button';
+import './App.css';
 
 class App extends Component {
+	constructor(props) {
+		super(props)
+		this.characters = this.importAll(require.context('../../media/characters', false, /.png$/));
+		this.karts = this.importAll(require.context('../../media/karts', false, /.png$/));
+		this.wheels = this.importAll(require.context('../../media/wheels', false, /.png$/));
+		this.gliders = this.importAll(require.context('../../media/gliders', false, /.png$/));
+
+		this.state = this.generateState()
+	}
+
+	generateState() {
+		return {
+			targetScore: Math.floor(Math.random() * 30) + 50,
+			character: this.characters[Math.floor(Math.random() * this.characters.length)],
+			kart: this.karts[Math.floor(Math.random() * this.karts.length)],
+			wheels: this.wheels[Math.floor(Math.random() * this.wheels.length)],
+			glider: this.gliders[Math.floor(Math.random() * this.gliders.length)]
+		}
+	}
+
+	importAll(r) {
+		return r.keys().map((item) => {
+			return { name: item.replace('.png', '').replace('./', ''), image: r(item) }
+		});
+	}
+
 	render() {
 		return (
-			<BrowserRouter>
-				<div>
-					<Menu />
-					<Switch>
-						<Route exact path='/' component={Conditions} />
-						<Route path='/score' component={Conditions} />
-					</Switch>
-				</div>
-			</BrowserRouter>
-		)
+			<div className="grid-container">
+				<Item primary className="score">
+					{this.state.targetScore}
+				</Item>
+				<Item primary className="character">
+					<img src={this.state.character.image} alt={this.state.character.name} />
+				</Item>
+				<Item className="kart">
+					<img src={this.state.kart.image} alt={this.state.kart.name} />
+				</Item>
+				<Item className="wheels">
+					<img src={this.state.wheels.image} alt={this.state.wheels.name} />
+				</Item>
+				<Item className="glider">
+					<img src={this.state.glider.image} alt={this.state.glider.name} />
+				</Item>
+				<Button className="footer" onClick={() => this.setState(this.generateState())}>Generate</Button>
+			</div>
+		);
 	}
 }
 
