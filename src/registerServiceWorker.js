e7251b089a8a77c8e06bb6597f7cf6c5994eeee9
Jakub Barszczewski <jakub.barszczewski@outlook.com>
2018-08-19 20:28:29 +0100

    added toastr

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -7,15 +7,16 @@
 
 // To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
 // This link also includes instructions on opting out of this behavior.
+import * as toastr from 'toastr'
 
 const isLocalhost = Boolean(
 	window.location.hostname === 'localhost' ||
-  // [::1] is the IPv6 localhost address.
-  window.location.hostname === '[::1]' ||
-  // 127.0.0.1/8 is considered localhost for IPv4.
-  window.location.hostname.match(
-  	/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-  )
+	// [::1] is the IPv6 localhost address.
+	window.location.hostname === '[::1]' ||
+	// 127.0.0.1/8 is considered localhost for IPv4.
+	window.location.hostname.match(
+		/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+	)
 )
 
 export default function register() {
@@ -41,7 +42,7 @@ export default function register() {
 				navigator.serviceWorker.ready.then(() => {
 					console.log(
 						'This web app is being served cache-first by a service ' +
-            'worker. To learn more, visit https://goo.gl/SC7cgQ'
+						'worker. To learn more, visit https://goo.gl/SC7cgQ'
 					)
 				})
 			} else {
@@ -66,6 +67,7 @@ function registerValidSW(swUrl) {
 							// It's the perfect time to display a "New content is
 							// available; please refresh." message in your web app.
 							console.log('New content is available; please refresh.')
+							toastr.info('New content is available; please refresh.')
 						} else {
 							// At this point, everything has been precached.
 							// It's the perfect time to display a
@@ -88,7 +90,7 @@ function checkValidServiceWorker(swUrl) {
 			// Ensure service worker exists, and that we really are getting a JS file.
 			if (
 				response.status === 404 ||
-        response.headers.get('content-type').indexOf('javascript') === -1
+				response.headers.get('content-type').indexOf('javascript') === -1
 			) {
 				// No service worker found. Probably a different app. Reload the page.
 				navigator.serviceWorker.ready.then(registration => {
